5eb1a9fbbadd61a1d6c1971581f9d088aae962bd
Ritsaert Hornstra <ritsaert@continuit.nl>
2025-02-28 00:17:12 +0100

    docs: Enhance clientConfigEnv documentation

--- src/client/clientConfigEnv.ts
+++ src/client/clientConfigEnv.ts
@@ -2,10 +2,16 @@ import type { ClientConfig } from './clientConfig.ts';
 import * as process from 'node:process';
 
 /**
- * Funcion that takes a ClientConfig object and returns a new ClientConfig object with the environment variables set.
+ * Funcion that takes a ClientConfig object and returns a new ClientConfig object augmented with the environment variables set.
  *
- * @param config
- * @returns
+ * @param config - The ClientConfig object to augment.
+ * @returns A new ClientConfig object with the environment variables set.
+ *
+ * @property {string} region is set from `AWS_REGION`, `AWS_DEFAULT_REGION`, `AMAZON_REGION` respectively if not already set
+ * @property {string} accessKeyId is set from `AWS_ACCESS_KEY_ID`, `AWS_ACCESS_KEY` respectively if not already set
+ * @property {string} secretAccessKey is set from `AWS_SECRET_ACCESS_KEY`, `AWS_SECRET_KEY` respectively if not already set
+ * @property {string} sessionToken is set from `AWS_SESSION_TOKEN` if present and not already set
+ * @property {string} endpoint is set from `AWS_ENDPOINT_URL` if present and not already set
  */
 export const clientConfigEnv = (config: ClientConfig): ClientConfig => {
   return {
