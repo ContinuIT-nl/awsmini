31390ae076025aeb98d8e6e76644d30e97dbcdce
Ritsaert Hornstra <ritsaert@continuit.nl>
2025-04-07 23:58:23 +0200

    fix: linter / formatter

--- src/lambda/lambda.ts
+++ src/lambda/lambda.ts
@@ -1,6 +1,11 @@
 import { AwsminiError } from '../misc/AwsminiError.ts';
 
-export const lambdaName = (name: string, region: string = '', accountId: string = '', version: string = 'v1'): string => {
+export const lambdaName = (
+  name: string,
+  region: string = '',
+  accountId: string = '',
+  version: string = 'v1',
+): string => {
   if (!name) throw new AwsminiError('Lambda name is required', 'lambda');
   // arn:aws:lambda:us-west-2:123456789012:function:my-function, max length 170 characters
   if (region && accountId) return `arn:aws:lambda:${region}:${accountId}:function:${name}`;
@@ -14,4 +19,3 @@ export const lambdaName = (name: string, region: string = '', accountId: string 
 // https://docs.aws.amazon.com/lambda/latest/api/API_CreateFunction.html
 // https://docs.aws.amazon.com/lambda/latest/api/API_DeleteFunction.html
 // https://docs.aws.amazon.com/lambda/latest/api/API_InvokeWithResponseStream.html
-
