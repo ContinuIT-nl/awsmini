90a81723e6bb38acd9570a82e2d48fb373e1dd76
Ritsaert Hornstra <ritsaert@continuit.nl>
2025-04-21 00:49:17 +0200

    chore: fix formatting

--- test/sqs.test.ts
+++ test/sqs.test.ts
@@ -106,10 +106,10 @@ Deno.test('sqs - send message batch', async () => {
     {
       id: '1',
       messageBody: 'Hello, world!',
-      messageAttributes: { 
-        test: 'test', 
-        test2: 2, 
-        test3: new Uint8Array(1) 
+      messageAttributes: {
+        test: 'test',
+        test2: 2,
+        test3: new Uint8Array(1),
       },
     },
     {
